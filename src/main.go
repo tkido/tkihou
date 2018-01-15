e3fb0866e691eaac0449c89c4e4044d96aadab9a
tkido <takanorikido@gmail.com>
2018-01-16 06:43:41 +0900

    watch

--- src/main.go
+++ src/main.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"sort"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
 )
 
-func main() {
+func getSource() string {
 	txts, err := filepath.Glob(path)
 	if err != nil {
 		log.Fatal(err)
@@ -18,7 +22,44 @@ func main() {
 		f2, _ := os.Stat(txts[j])
 		return f1.ModTime().After(f2.ModTime())
     })
-	fmt.Println(txts[0])
+	return txts[0]
+}
+
+func main() {
+	source := getSource()
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+	defer exec.Command(editor, source).Run()
+
+	err = watcher.Add(source)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Watching...")
+	updated := time.Now()
+	for {
+		select {
+		case ev := <-watcher.Events:
+			if ev.Op&fsnotify.Write != 0 {
+				now := time.Now()
+				fmt.Println(ev)
+				// "Write" event within 0.5 second to the same file is regarded as duplicated.
+				if now.Sub(updated) > time.Second/2 {
+					// convert
+					fmt.Println("Converted!!")
+					updated = now
+				}
+			}
+		case err = <-watcher.Errors:
+			log.Fatal(err)
+		}
+	}
+
 	
 	/*
 	f, err := os.Open(path)
