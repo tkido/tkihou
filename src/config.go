be4a42ee1d18d95b4ed909d83798aeec8bd5b0dc
tkido <takanorikido@gmail.com>
2018-01-16 09:42:52 +0900

    watch

--- src/config.go
+++ src/config.go
@@ -4,5 +4,8 @@ package main
 const (
 	chrome = `C:\Program Files (x86)\Google\Chrome\Application\chrome.exe`
 	editor = `C:\Program Files (x86)\Hidemaru\Hidemaru.exe`
+
 	path   = `C:\Users\takan\Dropbox\Kami Data\ブログ\*.txt`
+	result = "./result.txt"
+	html = "./result.html"
 )
--- src/main.go
+++ src/main.go
@@ -5,43 +5,37 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"os/signal"
 	"path/filepath"
 	"sort"
+	"syscall"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
 )
 
-func getSource() string {
-	txts, err := filepath.Glob(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	sort.Slice(txts, func(i, j int) bool {
-		f1, _ := os.Stat(txts[i])
-		f2, _ := os.Stat(txts[j])
-		return f1.ModTime().After(f2.ModTime())
-    })
-	return txts[0]
-}
-
 func main() {
 	source := getSource()
+	// convert()
+	exec.Command(chrome, html).Run()
 
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer watcher.Close()
-	defer exec.Command(editor, source).Run()
 
 	err = watcher.Add(source)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT)
+
 	log.Println("Watching...")
 	updated := time.Now()
+Loop:
 	for {
 		select {
 		case ev := <-watcher.Events:
@@ -50,16 +44,21 @@ func main() {
 				fmt.Println(ev)
 				// "Write" event within 0.5 second to the same file is regarded as duplicated.
 				if now.Sub(updated) > time.Second/2 {
-					// convert
+					// convert()
 					fmt.Println("Converted!!")
 					updated = now
 				}
 			}
 		case err = <-watcher.Errors:
 			log.Fatal(err)
+		case s := <- ch:
+			if s == syscall.SIGINT {
+				break Loop
+			}
 		}
 	}
-
+	fmt.Println("Loop End")
+	exec.Command(editor, result).Run()
 	
 	/*
 	f, err := os.Open(path)
@@ -78,4 +77,17 @@ func main() {
 	exec.Command(chrome, path).Run()
 	exec.Command(editor, path).Run()
 	*/
+}
+
+func getSource() string {
+	txts, err := filepath.Glob(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	sort.Slice(txts, func(i, j int) bool {
+		f1, _ := os.Stat(txts[i])
+		f2, _ := os.Stat(txts[j])
+		return f1.ModTime().After(f2.ModTime())
+    })
+	return txts[0]
 }
