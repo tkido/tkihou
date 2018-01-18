606b65bad789b317fde5f410100493f09a8710bc
Takanori Kido <tkido@xevo.com>
2018-01-18 17:17:50 +0900

    update

--- src/convert.go
+++ src/convert.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"html"
 	"log"
 	"os"
 	"regexp"
@@ -30,26 +32,49 @@ func convert(src string) {
 	buf := make([]string, 0, 128)
 	buf = append(buf, "<!--", title, "-->")
 
+	tmp := make([]string, 0)
+
 	for len(lines) != 0 {
 		line := lines[0]
 		switch {
 		case line == "":
 			lines = lines[1:]
-		case strings.HasPrefix(line, "===="):
-			buf = append(buf, "<hr />")
-			lines = lines[1:]
 		case strings.HasPrefix(line, "#"):
-			tmp := take(lines, "#")
+			tmp, lines = split(lines, "#")
 			buf = append(buf, "<!--")
-			buf = append(buf, tmp...)
+			buf = append(buf, mapString(tmp, pass)...)
 			buf = append(buf, "-->")
-			lines = lines[len(tmp):]
+		case strings.HasPrefix(line, "===="):
+			tmp, lines = split(lines, "====")
+			buf = append(buf, mapString(tmp, hr)...)
+		case strings.HasPrefix(line, "*"):
+			tmp, lines = split(lines, "*")
+			buf = append(buf, mapString(tmp, headline)...)
+		case strings.HasPrefix(line, "{{}}"): // notation
+			tmp, lines = split(lines, "{{}}")
+			buf = append(buf, mapString(tmp, pass)...)
+		case strings.HasPrefix(line, "{"):
+			tmp, lines = split(lines, "{")
+			buf = append(buf, mapString(tmp, divOpen)...)
+		case strings.HasPrefix(line, "}"):
+			tmp, lines = split(lines, "}")
+			buf = append(buf, mapString(tmp, divClose)...)
+		case strings.HasPrefix(line, ">>"):
+			tmp, lines = split(lines, ">>")
+			buf = append(buf, mapString(tmp, bqOpen)...)
+		case strings.HasPrefix(line, "<<"):
+			tmp, lines = split(lines, "<<")
+			buf = append(buf, mapString(tmp, bqClose)...)
+		case strings.HasPrefix(line, "\t"):
+			tmp, lines = split(lines, "\t")
+			buf = append(buf, "<pre><code>")
+			buf = append(buf, mapString(tmp, html.EscapeString)...)
+			buf = append(buf, "</pre></code>")
 		default:
-			tmp := takeParagraph(lines)
+			tmp, lines = splitParagraph(lines)
 			buf = append(buf, "<p>")
-			buf = append(buf, tmp...)
+			buf = append(buf, mapString(tmp, paragraph)...)
 			buf = append(buf, "</p>")
-			lines = lines[len(tmp):]
 		}
 	}
 
@@ -58,20 +83,79 @@ func convert(src string) {
 	execute(title, content)
 }
 
-func take(lines []string, mark string) []string {
+func split(lines []string, mark string) ([]string, []string) {
 	buf := make([]string, 0, 8)
-	for len(lines) != 0 && strings.HasPrefix(lines[0], mark) {
-		buf = append(buf, strings.Replace(lines[0], "#", "", 1))
-		lines = lines[1:]
+	for i, line := range lines {
+		if strings.HasPrefix(line, mark) {
+			buf = append(buf, strings.Replace(lines[i], mark, "", 1))
+		} else {
+			break
+		}
 	}
-	return buf
+	return buf, lines[len(buf):]
 }
 
-func takeParagraph(lines []string) []string {
+func splitParagraph(lines []string) ([]string, []string) {
 	buf := make([]string, 0, 8)
-	for len(lines) != 0 && !re.MatchString(lines[0]) {
-		buf = append(buf, lines[0])
-		lines = lines[1:]
+	for _, line := range lines {
+		if !re.MatchString(line) {
+			buf = append(buf, line)
+		} else {
+			break
+		}
 	}
-	return buf
+	return buf, lines[len(buf):]
+}
+
+func divOpen(line string) string {
+	switch {
+	case strings.Contains(line, "aa"):
+		return `<div class="ascii-art">`
+	case strings.Contains(line, "ep"):
+		return `<div class="epigraph">`
+	default:
+		return `<div>`
+	}
+}
+func divClose(line string) string {
+	return `</div>`
+}
+func bqOpen(line string) string {
+	return `<blockquote>`
+}
+func bqClose(line string) string {
+	return `</blockquote>`
+}
+func headline(line string) string {
+	level := min(3+strings.Count(line, "*"), 6)
+	content := strings.Replace(line, "*", "", -1)
+	return fmt.Sprintf("<h%d>%s</h%d>", level, content, level)
+}
+func hr(line string) string {
+	return "<hr />"
+}
+func paragraph(line string) string {
+	return line + `<br />`
+}
+func pass(line string) string {
+	return line
+}
+
+func inline(line string) string {
+	return line
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func mapString(x []string, f func(string) string) []string {
+	r := make([]string, len(x))
+	for i, e := range x {
+		r[i] = f(e)
+	}
+	return r
 }
