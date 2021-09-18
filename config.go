65a20e0344d112114a77c40615e96db6fa346195
tkido <tkido@xevo.com>
2021-09-18 09:48:26 +0900

    update config

--- config.go
+++ config.go
@@ -4,12 +4,12 @@ const (
 	chrome = `C:\Program Files (x86)\Google\Chrome\Application\chrome.exe`
 	editor = `C:\Program Files (x86)\Hidemaru\Hidemaru.exe`
 
-	watchPath = `C:\Users\takan\Dropbox\Kami Data\ブログ`
+	watchPath = `C:\Users\tkido\Dropbox\Kami Data\ブログ`
 	globPath  = watchPath + `\*.txt`
 	namiPath  = `C:\ols\Nami2000\temp.txt`
 
 	rstTxt      = "./result.txt"
-	rstHTML     = "./result.html"
+	rstHTML     = "./index.html"
 	tmplTkido   = "./tkido.html"
 	autoLinkTxt = "./auto_links.txt"
 )
